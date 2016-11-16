734fc4f629b8d083225a820c815f5c8f64838d12
dlozitskiy <dlozitskiy@deloitte.co.nz>
2016-11-17 09:20:03 +1300

    commit message

---
+++ manifests/fmw.pp
@@ -0,0 +1,178 @@
+# == Define: orawls::fmw
+#
+# installs FMW software like ADF, FORMS, OIM, WC, WCC, OSB, SOA Suite, B2B, MFT
+#
+##
+define orawls::fmw(
+  $version              = hiera('wls_version', 1111),        # 1036|1111|1211|1212|1213|1221|12211
+  $weblogic_home_dir    = hiera('wls_weblogic_home_dir'),    # /opt/oracle/middleware11gR1/wlserver_103
+  $middleware_home_dir  = hiera('wls_middleware_home_dir'),  # /opt/oracle/middleware11gR1
+  $oracle_base_home_dir = hiera('wls_oracle_base_home_dir'), # /opt/oracle
+  $jdk_home_dir         = hiera('wls_jdk_home_dir'),         # /usr/java/jdk1.7.0_45
+  $fmw_product          = undef,                             # adf|soa|soaqs|osb|wcc|wc|oim|oam|web|webgate|oud|mft|b2b|forms
+  $fmw_file             = undef,
+  $bpm                  = false,
+  $os_user              = hiera('wls_os_user'),              # oracle
+  $os_group             = hiera('wls_os_group'),             # dba
+  $download_dir         = hiera('wls_download_dir'),         # /data/install
+  $source               = hiera('wls_source', undef),        # puppet:///modules/orawls/ | /mnt | /vagrant
+  $remote_file          = true,                              # true|false
+  $log_output           = true,                             # true|false
+  $temp_directory       = hiera('wls_temp_dir','/tmp'),      # /tmp directory
+  $ohs_mode             = hiera('ohs_mode', 'collocated'),
+  $oracle_inventory_dir = undef,
+  $orainstpath_dir      = hiera('orainstpath_dir', undef),
+)
+{
+  $exec_path    = "${jdk_home_dir}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:"
+
+  if $oracle_inventory_dir == undef {
+    $oraInventory = "${oracle_base_home_dir}/oraInventory"
+  } else {
+    $oraInventory = $oracle_inventory_dir
+  }
+
+  if ( $orainstpath_dir == undef or $orainstpath_dir == '' ){
+        $oraInstPath = '/etc'
+      } 
+
+  #Sanitise the resource title so that it can safely be used in filenames and execs etc.
+  #After converting all spaces to underscores, remove all non alphanumeric characters (allow hypens and underscores too)
+  $convert_spaces_to_underscores = regsubst($title,'\s','_','G')
+  $sanitised_title = regsubst ($convert_spaces_to_underscores,'[^a-zA-Z0-9_-]','','G')
+
+  if ( $fmw_product == 'soa' ) {
+
+    if $version >= 1221 {
+      $total_files = 1
+      $fmw_silent_response_file = 'orawls/fmw_silent_soa_1221.rsp.erb'
+      $type                     = 'java'
+      if $bpm == true {
+        $install_type = 'BPM'
+      } else {
+        $install_type = 'SOA Suite'
+      }
+    }
+    elsif $version == 1213 {
+      $total_files = 1
+      $fmw_silent_response_file = 'orawls/fmw_silent_soa_1213.rsp.erb'
+      $type                     = 'java'
+      if $bpm == true {
+        $install_type = 'BPM'
+      } else {
+        $install_type = 'SOA Suite'
+      }
+    }
+
+  } elsif ( $fmw_product == 'osb' ) {
+
+    $total_files = 1
+    if $version >= 1221 {
+      $fmw_silent_response_file = 'orawls/fmw_silent_osb_1221.rsp.erb'
+      $type                     = 'java'
+    }
+    elsif $version == 1213 {
+      $fmw_silent_response_file = 'orawls/fmw_silent_osb_1213.rsp.erb'
+      $type                     = 'java'
+    }
+  }
+  
+  # check if the oracle home already exists, only for < 12.1.2, this is for performance reasons
+  if $version == 1212 or $version == 1213 or $version >= 1221 {
+    $continue = true
+  } else {
+    $found = orawls_oracle_exists($oracle_base_home_dir)
+
+    if $found == undef {
+      $continue = true
+    } else {
+      if ($found) {
+        $continue = false
+      } else {
+        notify { "orawls::fmw ${oracle_base_home_dir} does not exists": }
+        $continue = true
+      }
+    }
+  }
+
+  if ($continue) {
+
+    orawls::utils::orainst { "create oraInst for ${name}":
+      ora_inventory_dir => $oraInventory,
+      os_group          => $os_group,
+    }
+
+    file { "${download_dir}/${sanitised_title}_silent.rsp":
+      ensure  => present,
+      content => template($fmw_silent_response_file),
+      mode    => '0775',
+      owner   => $os_user,
+      group   => $os_group,
+      backup  => false,
+      require => Orawls::Utils::Orainst["create oraInst for ${name}"],
+    }
+
+    # for performance reasons, download and extract or just extract it
+    if $remote_file == true {
+      file { "${download_dir}/${fmw_file}":
+        ensure => file,
+        source => "${source}/${fmw_file}",
+        mode   => '0775',
+        owner  => $os_user,
+        group  => $os_group,
+        backup => false,
+        require => Orawls::Utils::Orainst["create oraInst for ${name}"],
+      }
+      $disk1_file = "${download_dir}/${fmw_file}"
+    } else {
+      $disk1_file = "${source}/${fmw_file}"
+    }
+
+    if $version >= 1221 {
+      $command = "-silent -responseFile ${download_dir}/${sanitised_title}_silent.rsp"
+    }
+    else {
+      $command = "-silent -response ${download_dir}/${sanitised_title}_silent.rsp -waitforcompletion"
+    }
+ 
+    if $version == 1212 or $version == 1213 or $version >= 1221 {
+    
+        $install = "java -Djava.io.tmpdir=${temp_directory} -jar "
+     
+        if ( $fmw_product == 'soa' ) {
+
+        exec { "install ${sanitised_title}":
+        command     => "${install}${download_dir}/${fmw_file} ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir}",
+        environment => "TEMP=${temp_directory}",
+        timeout     => 0,
+        creates     => "${middleware_home_dir}/soa",
+        cwd         => $temp_directory,
+        path        => $exec_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => [File["${download_dir}/${sanitised_title}_silent.rsp"],
+                        Orawls::Utils::Orainst["create oraInst for ${name}"],
+                        File["${download_dir}/${fmw_file}"],],
+        } 
+      } elsif ( $fmw_product == 'osb' ) {
+
+        exec { "install ${sanitised_title}":
+        command     => "${install}${download_dir}/${fmw_file} ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir}",
+        environment => "TEMP=${temp_directory}",
+        timeout     => 0,
+        creates     => "${middleware_home_dir}/osb",
+        cwd         => $temp_directory,
+        path        => $exec_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => [File["${download_dir}/${sanitised_title}_silent.rsp"],
+                        Orawls::Utils::Orainst["create oraInst for ${name}"],
+                        File["${download_dir}/${fmw_file}"],],
+
+      }
+     }
+    }
+  }
+}
---
+++ manifests/os.pp
@@ -0,0 +1,99 @@
+# == Class: orawls::os
+#
+class orawls::os (
+  $os_user              = 'oracle',              
+  $os_group             = 'oinstall',
+  $os_user_home			= '/home/oracle',
+  $os_user_password		= '$1$bgksg0Bc$gDICOuUpCl59noe0BZ5Y71',
+  $os_bin_root			= '/u01',
+  $os_packages			= ['binutils.x86_64','unzip.x86_64'],
+  $os_packages_rm		= ['java-1.7.0-openjdk.x86_64','java-1.6.0-openjdk.x86_64'],
+  $os_ssh_source		= undef,
+
+) {
+ 
+# Ensure that internal Linux Firewall (iptables) is disabled
+
+  service { iptables:
+        enable    => false,
+        ensure    => false,
+        hasstatus => true,
+  }
+
+# Ensure that the OS group exists on the Linux Server
+
+  group { $os_group:
+    ensure => present,
+  }
+
+# Ensure that the OS user exists on the Linux Server
+
+  user { $os_user:
+    ensure     => present,
+    groups     => $os_group,
+    shell      => '/bin/bash',
+    password   => $os_user_password,
+    home       => $os_user_home,
+    managehome => true,
+    require    => Group[$os_group],
+  }
+
+# Install required OS packages for OFM
+
+  package { $os_packages:
+    ensure  => present,
+  }
+
+# Remove Open JDK packages
+
+  package { $os_packages_rm:
+    ensure  => absent,
+  }
+
+# Create parent folder
+
+   file { "${os_bin_root}":
+    owner  => $os_user,
+    group  => $os_group,
+    mode   => "775",
+    ensure => "directory",
+   }
+
+# Download SSH keys for user
+
+   file { "${os_user_home}/.ssh/":
+    owner  => $os_user,
+    group  => $os_group,
+    mode   => "700",
+    ensure => "directory",
+    alias  => "ssh-dir",
+   }
+
+   file { "${os_user_home}/.ssh/id_rsa.pub":
+     ensure  => present,
+     owner   => $os_user,
+     group   => $os_group,
+     mode    => "644",
+     source  => "${os_ssh_source}/${os_user}_id_rsa.pub",
+     require => File["ssh-dir"],
+   }
+
+   file { "${os_user_home}/.ssh/id_rsa":
+     ensure  => present,
+     owner   => $os_user,
+     group   => $os_group,
+     mode    => "600",
+     source  => "${os_ssh_source}/${os_user}_id_rsa",
+     require => File["ssh-dir"],
+   }
+
+   file { "${os_user_home}/.ssh/authorized_keys":
+     ensure  => present,
+     owner   => $os_user,
+     group   => $os_group,
+     mode    => "644",
+     source  => "${os_ssh_source}/${os_user}_authorized_keys",
+     require => File["ssh-dir"],
+   }
+
+}
